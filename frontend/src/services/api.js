e4d99d863a4ed6708fd5189c980e81333dd9ca70
mohdzubairpatel <zubairpatel128@gmail.com>
2026-06-08 19:55:35 +0530

    Fix API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,9 @@
 import axios from "axios";
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
